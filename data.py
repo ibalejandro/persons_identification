7097c5ca631740da5d382a0c9c31e009c8a8b922
darkanita <darkanita@gmail.com>
2019-05-14 19:59:12 -0500

    Codigo Inicial

--- data.py
+++ data.py
@@ -1,7 +1,65 @@
 """ Todo lo que se necesitan para leer sus datos """
+
+from collections import defaultdict
+
+def read_pascalvoc(filepath):
+    pass
+
 def preparate_dataset():
+    #image,label = read_pascalvoc(image)
     raise NotImplementedError
 
+def preprocess_data():
+    pass
+
+def augment_image():
+    pass
+
+def make_dataset(sources, training=False, batch_size=1,
+    num_epochs=1, num_parallel_calls=1, shuffle_buffer_size=None):
+    """
+    Returns an operation to iterate over the dataset specified in sources
+
+    Args:
+        sources (list): A list of (lable_id, filepath) pairs.
+        training (bool): whether to apply certain processing steps
+            defined only in training mode (e.g. shuffle).
+        batch_size (int): number of elements the resulting tensor
+            should have.
+        num_epochs (int): Number of epochs to repeat the dataset.
+        num_parallel_calls (int): Number of parallel calls to use in
+            map operations.
+        shuffle_buffer_size (int): Number of elements from this dataset
+            from which the new dataset will sample.
+
+    Returns:
+        A tf.data.Dataset object. It will return a tuple images of shape
+        [N, H, W, CH] and labels shape [N, 1].
+    """
+    def load(row):
+        filepath = row['image']
+        img = tf.io.read_file(filepath)
+        img = tf.io.decode_jpeg(img)
+        return img, row['label']
+
+    if shuffle_buffer_size is None:
+        shuffle_buffer_size = batch_size*4
+
+    images, labels = zip(*sources)
+    ds = tf.data.Dataset.from_tensor_slices({
+        'image': list(images), 'label': list(labels)}) 
+
+    if training:
+        ds = ds.shuffle(shuffle_buffer_size)
+    
+    ds = ds.map(load, num_parallel_calls=num_parallel_calls)
+    ds = ds.map(lambda x,y: (preprocess_image(x), y))
+    ds = ds.map(lambda x,y: (augmentation_image(x), y))
+    ds = ds.repeat(count=num_epochs)
+    ds = ds.batch(batch_size=batch_size)
+    ds = ds.prefetch(1)
+
+    return ds
 
 if __name__ == "__main__":
     pass
