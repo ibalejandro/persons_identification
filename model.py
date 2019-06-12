5ffbb76c48a43b1c1e96f9269ea75639102a0d4e
darkanita <darkanita@gmail.com>
2019-06-11 20:03:13 -0500

    models

--- model.py
+++ model.py
@@ -1,65 +1,39 @@
-def LinearModel(input_shape=(32,32,3),activation='softmax'):
+import tensorflow as tf
+
+def get_lineal_model(num_class,img_shape=32,channels=3):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
-    model.add(tf.keras.layers.Dense(6, activation='softmax'))
-    model.compile(loss=tf.losses.SparseCategoricalCrossentropy(),optimizer=tf.optimizers.Adam(0.0001),metrics=['accuracy'])
+    model.add(tf.keras.layers.Flatten(input_shape=(img_shape,img_shape,channels)))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
     return model
 
+def get_letnet_model(num_class,img_shape=32,channels=3):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=6, kernel_size=(5,5), activation='tanh',padding='same', input_shape=(img_shape, img_shape, channels)))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Conv2D(16, (5,5), activation='tanh'))
+    model.add(tf.keras.layers.AveragePooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(120, (5,5), activation='tanh'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(84, activation='tanh'))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
 
-
-from tensorflow.contrib.layers import flatten
-
-def LeNet(x):    
-    # Hyperparameters
-    mu = 0
-    sigma = 0.1
-    layer_depth = {
-        'layer_1' : 6,
-        'layer_2' : 16,
-        'layer_3' : 120,
-        'layer_f1' : 84
-    }
-    
-    # TODO: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_w = tf.Variable(tf.truncated_normal(shape = [5,5,1,6],mean = mu, stddev = sigma))
-    conv1_b = tf.Variable(tf.zeros(6))
-    conv1 = tf.nn.conv2d(x,conv1_w, strides = [1,1,1,1], padding = 'VALID') + conv1_b 
-    # TODO: Activation.
-    conv1 = tf.nn.relu(conv1)
-
-    # TODO: Pooling. Input = 28x28x6. Output = 14x14x6.
-    pool_1 = tf.nn.max_pool(conv1,ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID')
-    
-    # TODO: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_w = tf.Variable(tf.truncated_normal(shape = [5,5,6,16], mean = mu, stddev = sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2 = tf.nn.conv2d(pool_1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b
-    # TODO: Activation.
-    conv2 = tf.nn.relu(conv2)
-
-    # TODO: Pooling. Input = 10x10x16. Output = 5x5x16.
-    pool_2 = tf.nn.max_pool(conv2, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'VALID') 
-    
-    # TODO: Flatten. Input = 5x5x16. Output = 400.
-    fc1 = flatten(pool_2)
-    
-    # TODO: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_w = tf.Variable(tf.truncated_normal(shape = (400,120), mean = mu, stddev = sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
-    fc1 = tf.matmul(fc1,fc1_w) + fc1_b
-    
-    # TODO: Activation.
-    fc1 = tf.nn.relu(fc1)
-
-    # TODO: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_w = tf.Variable(tf.truncated_normal(shape = (120,84), mean = mu, stddev = sigma))
-    fc2_b = tf.Variable(tf.zeros(84))
-    fc2 = tf.matmul(fc1,fc2_w) + fc2_b
-    # TODO: Activation.
-    fc2 = tf.nn.relu(fc2)
-    
-    # TODO: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_w = tf.Variable(tf.truncated_normal(shape = (84,10), mean = mu , stddev = sigma))
-    fc3_b = tf.Variable(tf.zeros(10))
-    logits = tf.matmul(fc2, fc3_w) + fc3_b
-    return logits
+def get_alexnet_model(num_class,img_shape=224,channels=3):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=96, kernel_size=(11,11), activation='relu',strides=(4,4), padding='valid', input_shape=(img_shape, img_shape, channels)))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(11,11), activation='relu',strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), activation='relu',strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), activation='relu',strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), activation='relu',strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(4096, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.4))
+    model.add(tf.keras.layers.Dense(4096, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.4))
+    model.add(tf.keras.layers.Dense(1000, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.4))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
